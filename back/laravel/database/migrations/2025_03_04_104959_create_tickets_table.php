96048de0c5350ca1b354c364dcddfc84336439b3
Eduu2004 <a20edurenlop@inspedralbes.cat>
2025-03-17 11:43:39 +0100

    actualitzar seients seeders

--- back/laravel/database/migrations/2025_03_04_104959_create_tickets_table.php
+++ back/laravel/database/migrations/2025_03_04_104959_create_tickets_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('email');
             $table->foreignId('session_id')->constrained('film_sessions')->onDelete('cascade');
             $table->json('seats'); 
             $table->integer('price');

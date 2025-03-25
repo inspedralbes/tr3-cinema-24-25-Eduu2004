d2e3744d5c1033f0330f9926d65f5cb5e10870b5
Eduu2004 <a20edurenlop@inspedralbes.cat>
2025-03-25 10:45:29 +0100

    rutes

--- back/laravel/routes/api.php
+++ back/laravel/routes/api.php
@@ -15,4 +15,8 @@ Route::get('/api/sessions/{sessionId}/tickets', [TicketsController::class, 'getS
 Route::post('/sessions/{id}/seats', [FilmSessionsController::class, 'reserveSeats']);
 Route::get('/sessions', [FilmSessionsController::class, 'index']); 
 Route::get('/movies', [MoviesController::class, 'index']); 
-
+Route::get('/tickets', [TicketsController::class, 'index']); 
+Route::post('/tickets', [TicketsController::class, 'store']); 
+Route::get('/sessions/{id}', [FilmSessionsController::class, 'show']);
+Route::get('/sessions/{id}/seats', [FilmSessionsController::class, 'seats']);
+Route::get('/movies/{id}', [MoviesController::class, 'show']);
